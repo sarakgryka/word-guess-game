4ade43fe396c43d7ab972a305e1b56f790898479
Sara Gryka <sarakgryka@hotmail.com>
2019-09-05 11:53:37 -0700

    game

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -7,12 +7,13 @@ let currentWord = wordsArray[Math.floor(Math.random() * (wordsArray.length))];
 
 // Game Variables//
 
-let guessesRemaining = 10
+
 let alreadyGuessed =[];
-let alreadyGuessedDisplay = document.getElementById("alreadyGuessed");
-let wins = 0;
-let losses = 0;
-let maxGuesses = document.getElementById("guessesRemaining");
+let alreadyGuessedDisplay = document.getElementById("alreadyGuessed"); 
+let wins = document.getElementById("wins").innerHTML = 0;
+let losses = document.getElementById("losses").innerHTML = 0;
+let maxGuesses = document.getElementById("guessesRemaining").innerHTML = 10;
+let guessesRemaining 
 let currentWordIndex;
 let answerArray = [];
 
@@ -20,11 +21,13 @@ let answerArray = [];
 
 
 let start = function (){
-
+    
     
     currentWordIndex = Math.floor(Math.random() * (wordsArray.length));
-    
+    maxGuesses = 10;
+    alreadyGuessed = [];
 
+    
     for (var i = 0; i < currentWord.length; i++) {
         answerArray[i] = " _ ";}
     
@@ -36,35 +39,58 @@ start();
 
 let guesses = function (letter) {
 
-    maxGuesses--;
+    
 
     if(currentWord.indexOf(letter)=== -1) {
 
         alreadyGuessed.push(letter);
-        alreadyGuessedDisplay.innerHTML = alreadyGuessed.join(",")
+        alreadyGuessedDisplay.innerHTML = alreadyGuessed.join(",");
+    
+        document.getElementById("guessesRemaining").innerHTML -- ;}
 
-    }
 
-    else {
+
+    else
         for (var j = 0; j < currentWord.length; j++) {
 
             if(currentWord[j] === letter) {
                 answerArray[j] = letter;
-                document.getElementById("currWord").innerHTML= answerArray.join("");
+                document.getElementById("currWord").innerHTML = answerArray.join("");
+                document.getElementById("guessesRemaining").innerHTML -- ;} }
+
+                
+        
+            
             }
-        }
-    }
-}
+        
+    
+
 
 
 
 document.onkeyup = function (event) {
-    guessesRemaining--;
+    
     var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
     guesses(letterGuessed);
+
+   
     
   };
 
+ //let checkWin = function () {
+
+ // if (answerArray.indexOf('_') === -1) {
+   //     document.getElementById("wins").innerHTML ++;
+        start();
+   // } else if (maxGuesses === 0) {
+   //  document.getElementById("losses").innerHTML ++;
+  //   start();
+   //  }
+
+     
+ // }
+ 
+//checkWin();
 //Get word//
 
 
